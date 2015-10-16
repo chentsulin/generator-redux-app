6f014fdf4453b3e495827392baae90aa601cbfb3
C. T. Lin <chentsulin@gmail.com>
2015-10-16 21:59:10 +0800

    tweak render import

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Root from './containers/Root';
 import { createHistory } from 'history';
 
@@ -7,7 +7,7 @@ import { createHistory } from 'history';
 const history = createHistory();
 
 
-ReactDOM.render(
+render(
   <Root history={history} />,
   document.getElementById('root')
 );
