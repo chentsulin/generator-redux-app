bdd6b9a7c61bdc4fc165bbd7f68a2283eadd435b
C.T. Lin <chentsulin@gmail.com>
2015-10-16 09:11:18 +0800

    update to react-redux@4

--- app/templates/server.js
+++ app/templates/server.js
@@ -7,7 +7,6 @@ var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
   publicPath: config.output.publicPath,
   stats: {
     colors: true
