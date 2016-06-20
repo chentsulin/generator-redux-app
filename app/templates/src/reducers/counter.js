a7608d14d90c00e55398ee8a4ed3938e1a7e757d
C. T. Lin <chentsulin@gmail.com>
2016-06-21 00:36:39 +0800

    Added createReducer utils (#43)

--- app/templates/src/reducers/counter.js
+++ app/templates/src/reducers/counter.js
@@ -1,16 +1,15 @@
+/* eslint-disable no-unused-vars */
+import createReducer from '../utils/createReducer';
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
 
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case INCREMENT_COUNTER:
-      return state + 1;
-    case DECREMENT_COUNTER:
-      return state - 1;
-    default:
-      return state;
-  }
-};
+const initialState = 0;
+
+const counter = createReducer(initialState, {
+  [INCREMENT_COUNTER]: (state, action) => state + 1,
+
+  [DECREMENT_COUNTER]: (state, action) => state - 1,
+});
 
 
 export default counter;
---
+++ app/templates/test/utils/createReducer.spec.js
@@ -0,0 +1,68 @@
+import { expect } from 'chai';
+import createReducer from '../../src/utils/createReducer';
+
+
+const INIT_ACTION = '@@test/INIT_ACTION';
+const ACTION = 'ACTION';
+
+const createNextStateHandler = (nextState) => ({
+  [ACTION]: () => nextState,
+});
+
+describe('utils', () => {
+  describe('createReducer', () => {
+    describe('handle number state', () => {
+      it('should return initial state when be called first time', () => {
+        const initialState = 1;
+        const reducer = createReducer(initialState, { [ACTION]: () => {} });
+        expect(reducer(undefined, { type: INIT_ACTION })).to.equal(initialState);
+      });
+
+      it('should return next state when receive action', () => {
+        const initialState = 1;
+        const nextState = 2;
+        const reducer = createReducer(initialState, createNextStateHandler(nextState));
+        const prevState = reducer(undefined, { type: INIT_ACTION });
+        expect(reducer(prevState, { type: ACTION })).to.equal(nextState);
+      });
+    });
+
+    describe('handle object state', () => {
+      it('should return initial state when be called first time', () => {
+        const initialState = {
+          key: 1,
+        };
+        const reducer = createReducer(initialState, { [ACTION]: () => {} });
+        expect(reducer(undefined, { type: INIT_ACTION })).to.equal(initialState);
+      });
+
+      it('should return next state when receive action', () => {
+        const initialState = {
+          key: 1,
+        };
+        const nextState = {
+          key: 2,
+        };
+        const reducer = createReducer(initialState, createNextStateHandler(nextState));
+        const prevState = reducer(undefined, { type: INIT_ACTION });
+        expect(reducer(prevState, { type: ACTION })).to.equal(nextState);
+      });
+    });
+
+    describe('handle array state', () => {
+      it('should return initial state when be called first time', () => {
+        const initialState = [1, 2, 3, 4];
+        const reducer = createReducer(initialState, { [ACTION]: () => {} });
+        expect(reducer(undefined, { type: INIT_ACTION })).to.equal(initialState);
+      });
+
+      it('should return next state when receive action', () => {
+        const initialState = [1, 2, 3, 4];
+        const nextState = [5, 6, 7, 8];
+        const reducer = createReducer(initialState, createNextStateHandler(nextState));
+        const prevState = reducer(undefined, { type: INIT_ACTION });
+        expect(reducer(prevState, { type: ACTION })).to.equal(nextState);
+      });
+    });
+  });
+});
