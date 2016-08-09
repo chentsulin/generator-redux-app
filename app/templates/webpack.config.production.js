0dffad4bca26729858c7594b4d075871d9264e9b
C. T. Lin <chentsulin@gmail.com>
2016-08-09 16:25:07 +0800

    use cheap-module-source-map instead of source-map (#55)
    In production

--- app/templates/webpack.config.production.js
+++ app/templates/webpack.config.production.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'cheap-module-source-map',
   entry: './src/index',
   output: {
     path: path.join(__dirname, 'dist'),
