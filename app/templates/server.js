f3f2f6cd010cb1a20ca873d5319c4b9c9e3b5f3e
C. T. Lin <chentsulin@gmail.com>
2016-08-10 22:44:40 +0800

    change lint files (#56)

--- app/templates/server.js
+++ app/templates/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
 const config = require('./webpack.config');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
